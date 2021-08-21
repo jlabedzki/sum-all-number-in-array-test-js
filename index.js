0959ef217dc347a04c2b80db92241f85dfdd3f02
Johnny <johnnylabedzki@hotmail.com>
2021-08-20 18:24:29 -0700

    Created a recursive function to add the sum of an array, including infinitely nested arrays.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = array => {
+  let sum = 0;
+  array.forEach(e => {
+    if (Array.isArray(e)) {
+      sum += sumItems(e);
+    } else {
+      sum += e;
+    }
+  });
+  return sum;
+};
+
+console.log(sumItems([1, 2, 3, 4, 4, 4, 5]));
 
 module.exports = sumItems;
